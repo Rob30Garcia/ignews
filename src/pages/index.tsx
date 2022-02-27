eef57cb8fc78fd788441da8a41182a2ce0aef4b1
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-02-27 19:03:58 -0400

    style: global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <div className={styles.title}>
+      <div>
         <h1>Hello</h1>
       </div>
     </>
