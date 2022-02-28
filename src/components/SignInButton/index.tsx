eb5ff96ffaacc04fbd83b0d6e3dcc697aa271009
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-02-28 16:32:00 -0400

    chore: add SignInButton loggedIn version

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,9 +1,23 @@
 import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  return (
+  const userIsLoggedIn = true;
+
+  return userIsLoggedIn ? (
+    <button
+      className={styles.signInButton} 
+      type="button" 
+    >
+      <FaGithub color='#04D361'/>
+      
+      Robert Garcia
+
+      <FiX color='#737380' className={styles.closeIcon}/>
+    </button>
+  ) : (
     <button
       className={styles.signInButton} 
       type="button" 
