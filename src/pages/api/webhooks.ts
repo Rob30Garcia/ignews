dca18828189fe2d02556b8cd3168128ba63c5ab2
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-03-07 20:46:11 -0400

    chore: listen events others

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -24,7 +24,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted'
 ]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -45,17 +47,30 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     //se precisar ser readable
     if(relevantEvents.has(type)) {
-      console.log('Evento recebido', event);
+      //console.log('Evento recebido', event);
 
       const checkoutSession = event.data.object as Stripe.Checkout.Session;
 
       try {
         switch(type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;            
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+
+            break;
+
           case 'checkout.session.completed':
           
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             );
 
             break;
